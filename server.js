078e69d33fe2b8245d5d2221f70e52149c8de690
RampageFTW <34176175+RampageFTW@users.noreply.github.com>
2018-04-05 17:30:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'postgres',
-    password : 'project',
+    user : '',
+    password : '',
     database : 'smart-brain'
   }
 });
